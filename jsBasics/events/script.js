9bcc594ab46529083f7e12157e5309eea997526d
JK-B0T <JK-B0T@users.noreply.github.com>
2024-03-08 13:00:29 +0100

    Add more examples to events in JSbasics

--- jsBasics/events/script.js
+++ jsBasics/events/script.js
@@ -15,3 +15,26 @@ btn2.onclick = button2;
 
 const btn3 = document.querySelector("#btn3");
 btn3.addEventListener("click", button3, false);
+
+const btn = document.querySelector("#btn");
+btn.addEventListener('click', function (e) {
+    //console.log(e);
+    if (e.target.style.background != 'blue') {
+        e.target.style.background = 'blue';
+        e.target.style.color = 'white';
+    } else {
+        e.target.style.background = 'red';
+        e.target.style.color = 'black';
+    }
+});
+
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    console.log(button.id);
+  });
+});
